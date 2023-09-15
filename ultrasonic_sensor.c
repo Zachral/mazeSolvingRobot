41a07e4b02209d22aae0d67db0c8b074240edfb4
Zachral <soderberg.danne@gmail.com>
2023-09-15 11:35:50 +0200

    added code for ultrasonic sensors

---
+++ ultrasonic_sensor.c
@@ -0,0 +1,58 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include "ultrasonic_sensor.h"
+
+
+unsigned int echo_duration;
+
+void init_Ultrasonic_sensor(void)
+{
+  Ultrasonic_Triger_Port_Direction |= (Left_Ultrasonic_Trigger_pin);    // Ultrasonic Trigger pin
+  Ultrasonic_Triger_Port &= ~(Left_Ultrasonic_Trigger_pin);   // Ultrasonic pin set to low
+  
+  Ultrasonic_Triger_Port_Direction |= (Right_Ultrasonic_Trigger_pin);    // Ultrasonic Trigger pin
+  Ultrasonic_Triger_Port &= ~(Right_Ultrasonic_Trigger_pin);   // Ultrasonic pin set to low
+
+  Ultrasonic_Triger_Port_Direction |= (Front_Ultrasonic_Trigger_pin);    // Ultrasonic Trigger pin
+  Ultrasonic_Triger_Port &= ~(Front_Ultrasonic_Trigger_pin);   // Ultrasonic pin set to low
+
+  Ultrasonic_Echo_Port_Direction &= ~(Left_Ultrasonic_Echo_pin);   // Ultrasonic Echo pin set to input
+  Ultrasonic_Echo_Port |= Left_Ultrasonic_Echo_pin;      // Enable pullup
+
+  Ultrasonic_Echo_Port_Direction &= ~(Right_Ultrasonic_Echo_pin);   // Ultrasonic Echo pin set to input
+  Ultrasonic_Echo_Port |= Right_Ultrasonic_Echo_pin;      // Enable pullup
+
+  Ultrasonic_Echo_Port_Direction &= ~(Front_Ultrasonic_Echo_pin);   // Ultrasonic Echo pin set to input
+  Ultrasonic_Echo_Port |= Front_Ultrasonic_Echo_pin;      // Enable pullup
+}
+
+void trigger_Ultrasonic_sensor(void)
+{
+    Ultrasonic_Triger_Port |= (Left_Ultrasonic_Trigger_pin);
+    Ultrasonic_Triger_Port |= (Right_Ultrasonic_Trigger_pin);
+     Ultrasonic_Triger_Port |= (Front_Ultrasonic_Trigger_pin);
+    _delay_us(10);
+    Ultrasonic_Triger_Port &= ~(Left_Ultrasonic_Trigger_pin);
+    Ultrasonic_Triger_Port &= ~(Right_Ultrasonic_Trigger_pin);
+    Ultrasonic_Triger_Port &= ~(Front_Ultrasonic_Trigger_pin);
+}
+
+unsigned int read_Ultrasonic_sensor(unsigned int ultrasonicEchoPin)
+{
+    echo_duration = 0;   
+    while((Ultrasonic_Echo_Input&ultrasonicEchoPin) != ultrasonicEchoPin);
+    while(((Ultrasonic_Echo_Input&ultrasonicEchoPin) == ultrasonicEchoPin) && (echo_duration<20000))
+    {
+      _delay_us(1);
+      echo_duration++;
+    }
+    return echo_duration;
+}
+
+
+unsigned int get_distance_Ultrasonic_sensor(unsigned int ultrasonicEchoPin)
+{
+  trigger_Ultrasonic_sensor();
+  return (read_Ultrasonic_sensor(ultrasonicEchoPin)/SCALE_FACTOR) + 20;
+  ;
+}
---
+++ ultrasonic_sensor.h
@@ -0,0 +1,47 @@
+#ifndef _ULTRASONIC_SENSOR_H___
+#define _ULTRASONIC_TRIGGER_ECHO_H___
+
+#include "common_defines.h"
+
+
+#define    BIT7    0x80
+#define    BIT6    0x40
+#define    BIT5    0x20
+#define    BIT4    0x10
+#define    BIT3    0x08
+#define    BIT2    0x04
+#define    BIT1    0x02
+#define    BIT0    0x01
+
+/*Need to change according to connections*/
+#define ULTRASONIC_PORT_IND             C            
+#define LEFT_ULTRASONIC_ECHO            0            
+#define LEFT_ULTRASONIC_TRIGGER         1            
+#define FRONT_ULTRASONIC_ECHO           2            
+#define FRONT_ULTRASONIC_TRIGGER        3            
+#define RIGHT_ULTRASONIC_ECHO           4            
+#define RIGHT_ULTRASONIC_TRIGGER        5     
+
+//Change to calibrate sensor distance
+#define SCALE_FACTOR      2.5
+
+
+#define Ultrasonic_Triger_Port_Direction            DDR_(ULTRASONIC_PORT_IND)
+#define Ultrasonic_Triger_Port                      PORT_(ULTRASONIC_PORT_IND)
+#define Left_Ultrasonic_Trigger_pin                  BIT_(LEFT_ULTRASONIC_TRIGGER)
+#define Right_Ultrasonic_Trigger_pin                  BIT_(RIGHT_ULTRASONIC_TRIGGER)
+#define Front_Ultrasonic_Trigger_pin                  BIT_(FRONT_ULTRASONIC_TRIGGER)
+
+#define Ultrasonic_Echo_Port_Direction              DDR_(ULTRASONIC_PORT_IND)
+#define Ultrasonic_Echo_Port                        PORT_(ULTRASONIC_PORT_IND)
+#define Ultrasonic_Echo_Input                       PIN_(ULTRASONIC_PORT_IND)
+#define Left_Ultrasonic_Echo_pin                    BIT_(LEFT_ULTRASONIC_ECHO)
+#define Right_Ultrasonic_Echo_pin                    BIT_(RIGHT_ULTRASONIC_ECHO)
+#define Front_Ultrasonic_Echo_pin                    BIT_(FRONT_ULTRASONIC_ECHO)
+
+void init_Ultrasonic_sensor(void);
+void trigger_Ultrasonic_sensor(void);
+unsigned int read_Ultrasonic_sensor(unsigned int ultrasonicEchoPin);
+unsigned int get_distance_Ultrasonic_sensor(unsigned int ultrasonicEchoPin);
+
+#endif
