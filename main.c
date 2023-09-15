cf32476d10d098f3185790cfd750f9266416820d
Zachral <soderberg.danne@gmail.com>
2023-09-15 16:10:15 +0200

    calibrated

--- main.c
+++ main.c
@@ -9,16 +9,20 @@
 #include "drive.h"
 #include "ultrasonic_sensor.h"
 
-int main(void){
+void setup(){
     _delay_ms(1000);
     init_servo();
     button_init(); 
     init_serial();
     init_Ultrasonic_sensor();
     sei();
-    static volatile bool buttonWasPressed = false; 
-    int frontDistance = 0; 
+    return; 
+}
 
+int main(void){
+    setup();
+    static volatile bool buttonWasPressed = false; 
+    int frontDistance = 0, leftDistance = 0, rightDistance = 0; 
 
     while(!buttonWasPressed){
          buttonClick(&buttonWasPressed);
@@ -26,14 +30,12 @@ int main(void){
     
     while(1){
         drive_forward();
-        frontDistance = get_distance_Ultrasonic_sensor(Front_Ultrasonic_Echo_pin); 
-
+        frontDistance = get_distance_Ultrasonic_sensor(Left_Ultrasonic_Echo_pin);
         if(convert_ultrasonic_input_to_centimeters(frontDistance) < 9){
             stop();
-            _delay_ms(350);
-            turn_left(); 
-        };
-       
+            _delay_ms(1000); 
+        }
+
     }
     return 0;
 }
