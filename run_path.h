fca48ea9190c046171ff82e2017c7071fe168d55
Zachral <soderberg.danne@gmail.com>
2023-09-27 11:38:34 +0200

    added structs

--- run_path.h
+++ run_path.h
@@ -1,5 +1,24 @@
 #ifndef _RUN_PATH_H__
 #define _RUN_PATH_H__
 
+#include "millis.h"
+
+typedef struct{
+    char currentDirection; 
+    volatile millis_t timeSinceDirectionChange;
+}direction_and_time_of_turn_t; 
+
+typedef struct{
+    direction_and_time_of_turn_t currentRunPath[50]; 
+}current_run_path;
+
+
+typedef enum {
+    LEFT_TURN,
+    RIGHT_TURN,
+    STRAIGHT, 
+    U_TURN
+}driving_direction_t; 
+
 void decide_path( int frontDistance, int leftDistance, int rightDistance); 
 #endif
